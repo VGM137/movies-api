e09abb1a056f7f49055120d97219ef5f551f113a
Victor <victorgestaltung@gmail.com>
2021-09-05 19:35:25 -0500

    Authentication strategies

--- ssr-server/config/index.js
+++ ssr-server/config/index.js
@@ -4,7 +4,11 @@ const config = {
   dev: process.env.NODE_ENV !== 'production',
   port: process.env.PORT || 8000,
   apiUrl: process.env.API_URL,
-  apiKeyToken: process.env.API_KEY_TOKEN
+  apiKeyToken: process.env.API_KEY_TOKEN,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookClientId: process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET
 };
 
 module.exports = { config: config };
--- ssr-server/index.js
+++ ssr-server/index.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const passport = require('passport');
+const helmet = require('helmet')
+const boom = require('@hapi/boom');
+const cookieParser = require('cookie-parser');
+const axios = require('axios');
 
 const { config } = require("./config");
 
@@ -6,14 +11,71 @@ const app = express();
 
 // body parser
 app.use(express.json());
+app.use(helmet())
+app.use(cookieParser())
+
+//Basic Strategy
+require('./utils/auth/strategies/basic');
+
+const THIRTY_DAYS_IN_SEC = 2592000;
+const TWO_HOURS_IN_SEC = 7200;
+
+//OAuth strategy
+/* require('./utils/auth/strategies/oauth') */
+require('./utils/auth/strategies/google')
+require('./utils/auth/strategies/facebook')
 
 //Sesión
 app.post("/auth/sign-in", async function(req, res, next) {
+  console.log(`This is de req from sig-in: ${req}`)
+  const { rememberMe } = req.body;
+  
+  passport.authenticate('basic', function(error, data){
+    
+    try{
+      if(error || !data){
+        console.log(error)
+        console.log(data)
+        next(boom.unauthorized());
+      }
+      
+      const { token, ...user } = data;
+      console.log(data)
 
+      req.login(data, { session: false }, async function(error){
+          if(error){
+            next(error);
+          }
+
+        res.cookie('token', token, {
+          httpOnly: !config.dev,
+          secure: !config.dev,
+          maxAge: rememberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC
+        })
+
+        res.status(200).json(user);
+      });
+
+    }catch(error){
+      next(error)
+    }
+  })(req, res, next)
 });
 
 app.post("/auth/sign-up", async function(req, res, next) {
+  const { body: user } = req;
 
+  try{
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: 'post',
+      data: user
+    })
+
+    res.status(201).json({ message: 'user created' })
+  }catch (error) {
+    next(error)
+  }
 });
 
 //Movies API
@@ -23,12 +85,119 @@ app.get("/movies", async function(req, res, next) {
 
 //User movies
 app.post("/user-movies", async function(req, res, next) {
+  try {
+    const { body: userMovie } = req
+    const { token } = req.cookies
+    console.log(userMovie)
 
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: 'post',
+      data: userMovie
+    });
+
+    if(status !== 201){
+      return next(boom.badImplementation());
+    }
+
+    res.status(201).json(data);
+
+  }catch (error){
+    next(error)
+  }
 });
 
 app.delete("/user-movies/:userMovieId", async function(req, res, next) {
+  try {
+    const { userMovieId } = req.params;
+    const { token } = req.cookies;
 
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies/${userMovieId}`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: 'delete',
+    });
+
+    if(status !== 200){
+      return next(boom.badImplementation());
+    }
+
+    res.status(200).json(data);
+  }catch (error){
+    next(error)
+  }
 });
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["openid", "profile","email" ]
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { session: false }),
+  function(req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie("token", token, {
+      httpOnly: !config.dev,
+      secure: !config.dev
+    });
+
+    res.status(200).json(user);
+  }
+);
+
+/* app.get('/auth/google-oauth', passport.authenticate("google-oauth", {
+  scope: ['email', 'profile', 'openid']
+}));
+
+app.get(
+  "/auth/google-oauth/callback", 
+  passport.authenticate("google-oauth", { session: false }),
+  function(req, res, next){
+    if(!req.user){
+      next(boom.unauthorized());
+    }
+    const { token, ...user } = req.user;
+
+    res.cookie("token", token, {
+      httpOnly: !config.dev,
+      secure: !config.dev
+    })
+
+    res.status(200).json(user)
+  }
+); */
+
+app.get("/auth/facebook", passport.authenticate("facebook"));
+
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", { session: false }),
+  function(req, res, next) {
+    
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie("token", token, {
+      httpOnly: !config.dev,
+      secure: !config.dev
+    });
+
+    res.status(200).json(user);
+  }
+);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
